6d86bd9106aa01d36272d9a7fef6ea875ba1f695
Demomomomo <2889890021@qq.com>
2023-01-15 19:46:49 +0800

    e

--- src/.vuepress/.temp/pages/Algorithm/图论/cha-fen-yue-shu.html.vue
+++ src/.vuepress/.temp/pages/Algorithm/图论/cha-fen-yue-shu.html.vue
@@ -1,3 +1,6 @@
-<template><div></div></template>
+<template><div><h2 id="作用" tabindex="-1"><a class="header-anchor" href="#作用" aria-hidden="true">#</a> 作用：</h2>
+<p>（1）求不等式的可行解<br>
+（2）求最大值或者最小值</p>
+</div></template>
 
 
