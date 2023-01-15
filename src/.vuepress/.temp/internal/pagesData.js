62781a15100359ad676fc9bf5054306d825f4762
Demomomomo <2889890021@qq.com>
2023-01-15 23:24:28 +0800

    e

--- src/.vuepress/.temp/internal/pagesData.js
+++ src/.vuepress/.temp/internal/pagesData.js
@@ -7,6 +7,8 @@ export const pagesData = {
   "v-799e9206": () => import(/* webpackChunkName: "v-799e9206" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/index.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/cha-fen-yue-shu.html
   "v-7aebf5c4": () => import(/* webpackChunkName: "v-7aebf5c4" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/cha-fen-yue-shu.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%9B%BE%E8%AE%BA/duo-yuan-uiduanlu.html
+  "v-0eb212bb": () => import(/* webpackChunkName: "v-0eb212bb" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/duo-yuan-uiduanlu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/er-fen-tu.html
   "v-27f3ede8": () => import(/* webpackChunkName: "v-27f3ede8" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/er-fen-tu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/lca.html
@@ -25,12 +27,12 @@ export const pagesData = {
   "v-2a37981f": () => import(/* webpackChunkName: "v-2a37981f" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/kmp.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/manachar.html
   "v-057f5b88": () => import(/* webpackChunkName: "v-057f5b88" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/manachar.html.js").then(({ data }) => data),
-  // path: /algorithm/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/bing-cha-ji.html
-  "v-8e8bac92": () => import(/* webpackChunkName: "v-8e8bac92" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数据结构/bing-cha-ji.html.js").then(({ data }) => data),
   // path: /solution/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/bei-bao.html
   "v-9d49b844": () => import(/* webpackChunkName: "v-9d49b844" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/动态规划/bei-bao.html.js").then(({ data }) => data),
   // path: /solution/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/dp.html
   "v-0db0ff5f": () => import(/* webpackChunkName: "v-0db0ff5f" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/动态规划/dp.html.js").then(({ data }) => data),
+  // path: /algorithm/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/bing-cha-ji.html
+  "v-8e8bac92": () => import(/* webpackChunkName: "v-8e8bac92" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数据结构/bing-cha-ji.html.js").then(({ data }) => data),
   // path: /solution/%E5%9B%BE%E8%AE%BA/qiang-lian-tong-fen-liang.html
   "v-1b93fb49": () => import(/* webpackChunkName: "v-1b93fb49" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/图论/qiang-lian-tong-fen-liang.html.js").then(({ data }) => data),
   // path: /solution/%E5%9B%BE%E8%AE%BA/shu.html
@@ -41,6 +43,10 @@ export const pagesData = {
   "v-59ad23c3": () => import(/* webpackChunkName: "v-59ad23c3" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/字符串/kmp.html.js").then(({ data }) => data),
   // path: /solution/%E5%AD%97%E7%AC%A6%E4%B8%B2/zi-fu-chuan.html
   "v-07f2a534": () => import(/* webpackChunkName: "v-07f2a534" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/字符串/zi-fu-chuan.html.js").then(({ data }) => data),
+  // path: /solution/%E6%90%9C%E7%B4%A2/bfs.html
+  "v-267368ef": () => import(/* webpackChunkName: "v-267368ef" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/bfs.html.js").then(({ data }) => data),
+  // path: /solution/%E6%90%9C%E7%B4%A2/dfs.html
+  "v-13947aa6": () => import(/* webpackChunkName: "v-13947aa6" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/dfs.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E5%AD%A6/fei-bo-na-qi-shu-lie.html
   "v-156bc363": () => import(/* webpackChunkName: "v-156bc363" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数学/fei-bo-na-qi-shu-lie.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E5%AD%A6/ju-zhen-kuai-su-mi.html
@@ -61,12 +67,20 @@ export const pagesData = {
   "v-14b99028": () => import(/* webpackChunkName: "v-14b99028" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/shu-zhuang-shu-zu---xian-duan-shu.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/xian-duan-shu.html
   "v-279cebf0": () => import(/* webpackChunkName: "v-279cebf0" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/xian-duan-shu.html.js").then(({ data }) => data),
+  // path: /solution/%E6%9D%82%E8%AE%B0/bi-ji.html
+  "v-1d691529": () => import(/* webpackChunkName: "v-1d691529" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/杂记/bi-ji.html.js").then(({ data }) => data),
   // path: /solution/%E6%AF%94%E8%B5%9B/abc.html
   "v-11413ec7": () => import(/* webpackChunkName: "v-11413ec7" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/比赛/abc.html.js").then(({ data }) => data),
   // path: /solution/%E6%AF%94%E8%B5%9B/ccpc-icpc.html
   "v-27efaafc": () => import(/* webpackChunkName: "v-27efaafc" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/比赛/ccpc-icpc.html.js").then(({ data }) => data),
   // path: /solution/%E6%AF%94%E8%B5%9B/cf.html
   "v-348f8f46": () => import(/* webpackChunkName: "v-348f8f46" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/比赛/cf.html.js").then(({ data }) => data),
+  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/jie-gou-ti-pai-xu.html
+  "v-7c8a5a6c": () => import(/* webpackChunkName: "v-7c8a5a6c" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/jie-gou-ti-pai-xu.html.js").then(({ data }) => data),
+  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/map.html
+  "v-187be613": () => import(/* webpackChunkName: "v-187be613" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/map.html.js").then(({ data }) => data),
+  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/stl.html
+  "v-6e05f6a4": () => import(/* webpackChunkName: "v-6e05f6a4" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/stl.html.js").then(({ data }) => data),
   // path: /solution/%E7%AE%97%E6%B3%95%E6%8A%80%E5%B7%A7/er-fen.html
   "v-cab1018e": () => import(/* webpackChunkName: "v-cab1018e" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/算法技巧/er-fen.html.js").then(({ data }) => data),
   // path: /solution/%E7%AE%97%E6%B3%95%E6%8A%80%E5%B7%A7/gou-zao.html
@@ -75,18 +89,6 @@ export const pagesData = {
   "v-5f8a0336": () => import(/* webpackChunkName: "v-5f8a0336" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/算法技巧/pai-xu.html.js").then(({ data }) => data),
   // path: /solution/%E7%AE%97%E6%B3%95%E6%8A%80%E5%B7%A7/si-wei.html
   "v-06000b64": () => import(/* webpackChunkName: "v-06000b64" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/算法技巧/si-wei.html.js").then(({ data }) => data),
-  // path: /solution/%E6%9D%82%E8%AE%B0/bi-ji.html
-  "v-1d691529": () => import(/* webpackChunkName: "v-1d691529" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/杂记/bi-ji.html.js").then(({ data }) => data),
-  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/jie-gou-ti-pai-xu.html
-  "v-7c8a5a6c": () => import(/* webpackChunkName: "v-7c8a5a6c" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/jie-gou-ti-pai-xu.html.js").then(({ data }) => data),
-  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/map.html
-  "v-187be613": () => import(/* webpackChunkName: "v-187be613" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/map.html.js").then(({ data }) => data),
-  // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/stl.html
-  "v-6e05f6a4": () => import(/* webpackChunkName: "v-6e05f6a4" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/stl.html.js").then(({ data }) => data),
-  // path: /solution/%E6%90%9C%E7%B4%A2/bfs.html
-  "v-267368ef": () => import(/* webpackChunkName: "v-267368ef" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/bfs.html.js").then(({ data }) => data),
-  // path: /solution/%E6%90%9C%E7%B4%A2/dfs.html
-  "v-13947aa6": () => import(/* webpackChunkName: "v-13947aa6" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/dfs.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"E:/TechBlog/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
   // path: /category/
@@ -111,20 +113,20 @@ export const pagesData = {
   "v-300d34ce": () => import(/* webpackChunkName: "v-300d34ce" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/图/index.html.js").then(({ data }) => data),
   // path: /tag/%E5%AD%97%E7%AC%A6%E4%B8%B2/
   "v-93cb9d6e": () => import(/* webpackChunkName: "v-93cb9d6e" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/字符串/index.html.js").then(({ data }) => data),
+  // path: /tag/%E6%90%9C%E7%B4%A2/
+  "v-3b0e2518": () => import(/* webpackChunkName: "v-3b0e2518" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/搜索/index.html.js").then(({ data }) => data),
   // path: /tag/%E6%95%B0%E5%AD%A6/
   "v-9904a07c": () => import(/* webpackChunkName: "v-9904a07c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/数学/index.html.js").then(({ data }) => data),
   // path: /tag/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/
   "v-13770b26": () => import(/* webpackChunkName: "v-13770b26" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/数据结构/index.html.js").then(({ data }) => data),
-  // path: /tag/acm/
-  "v-b316775c": () => import(/* webpackChunkName: "v-b316775c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/acm/index.html.js").then(({ data }) => data),
-  // path: /tag/%E6%8A%80%E5%B7%A7/
-  "v-b7d62b20": () => import(/* webpackChunkName: "v-b7d62b20" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/技巧/index.html.js").then(({ data }) => data),
   // path: /tag/note/
   "v-28e08cef": () => import(/* webpackChunkName: "v-28e08cef" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/note/index.html.js").then(({ data }) => data),
+  // path: /tag/acm/
+  "v-b316775c": () => import(/* webpackChunkName: "v-b316775c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/acm/index.html.js").then(({ data }) => data),
   // path: /tag/%E8%AF%AD%E8%A8%80%E5%9F%BA%E7%A1%80/
   "v-b7e505f4": () => import(/* webpackChunkName: "v-b7e505f4" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/语言基础/index.html.js").then(({ data }) => data),
   // path: /tag/stl/
   "v-b3059a9c": () => import(/* webpackChunkName: "v-b3059a9c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/stl/index.html.js").then(({ data }) => data),
-  // path: /tag/%E6%90%9C%E7%B4%A2/
-  "v-3b0e2518": () => import(/* webpackChunkName: "v-3b0e2518" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/搜索/index.html.js").then(({ data }) => data),
+  // path: /tag/%E6%8A%80%E5%B7%A7/
+  "v-b7d62b20": () => import(/* webpackChunkName: "v-b7d62b20" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/技巧/index.html.js").then(({ data }) => data),
 }
---
+++ src/.vuepress/.temp/pages/Algorithm/图论/duo-yuan-uiduanlu.html.vue
@@ -0,0 +1,34 @@
+<template><div><h2 id="概念" tabindex="-1"><a class="header-anchor" href="#概念" aria-hidden="true">#</a> 概念</h2>
+<p>起点和终点都不确定的最短路<br>
+时间复杂度O（n* n *n）<br>
+思路：
+用邻接矩阵来存储边d[i][j]存i到j的边的长度<br>
+三重循环：<br>
+第一重循环循环中间点，第二第三重循环循环两边点</p>
+<div class="language-cpp line-numbers-mode" data-ext="cpp"><pre v-pre class="language-cpp"><code><span class="token comment">//初始化：</span>
+
+<span class="token function">memset</span><span class="token punctuation">(</span>d<span class="token punctuation">,</span><span class="token number">0x3f</span><span class="token punctuation">,</span><span class="token keyword">sizeof</span> d<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">for</span><span class="token punctuation">(</span><span class="token keyword">int</span> i<span class="token operator">=</span><span class="token number">1</span><span class="token punctuation">;</span>i<span class="token operator">&lt;=</span>n<span class="token punctuation">;</span>i<span class="token operator">++</span><span class="token punctuation">)</span>d<span class="token punctuation">[</span>i<span class="token punctuation">]</span><span class="token punctuation">[</span>i<span class="token punctuation">]</span><span class="token operator">=</span><span class="token number">0</span><span class="token punctuation">;</span>
+
+<span class="token comment">//求最短路</span>
+
+	<span class="token keyword">for</span><span class="token punctuation">(</span><span class="token keyword">int</span> k<span class="token operator">=</span><span class="token number">1</span><span class="token punctuation">;</span>k<span class="token operator">&lt;=</span>n<span class="token punctuation">;</span>k<span class="token operator">++</span><span class="token punctuation">)</span> 
+		<span class="token keyword">for</span><span class="token punctuation">(</span><span class="token keyword">int</span> i<span class="token operator">=</span><span class="token number">1</span><span class="token punctuation">;</span>i<span class="token operator">&lt;=</span>n<span class="token punctuation">;</span>i<span class="token operator">++</span><span class="token punctuation">)</span>
+			<span class="token keyword">for</span><span class="token punctuation">(</span><span class="token keyword">int</span> j<span class="token operator">=</span><span class="token number">1</span><span class="token punctuation">;</span>j<span class="token operator">&lt;=</span>n<span class="token punctuation">;</span>j<span class="token operator">++</span><span class="token punctuation">)</span>
+				d<span class="token punctuation">[</span>i<span class="token punctuation">]</span><span class="token punctuation">[</span>j<span class="token punctuation">]</span><span class="token operator">=</span><span class="token function">min</span><span class="token punctuation">(</span>d<span class="token punctuation">[</span>i<span class="token punctuation">]</span><span class="token punctuation">[</span>j<span class="token punctuation">]</span><span class="token punctuation">,</span>d<span class="token punctuation">[</span>i<span class="token punctuation">]</span><span class="token punctuation">[</span>k<span class="token punctuation">]</span><span class="token operator">+</span>d<span class="token punctuation">[</span>k<span class="token punctuation">]</span><span class="token punctuation">[</span>j<span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>对状态的分析：
+d[k][i][j]所以从i出发，最终走到j，且中间只经过节点编号不超过k的点的所有路径的最短路径
+分成两部分：<br>
+不包含编号为k的点的路径：d[k-1][i][j]<br>
+包含编号为k的点的路径:那么路径就是i-&gt;k-&gt;j:<br>
+对于i-&gt;k这段路径，因为题目限制了经过节点编号不超过k，所以他的状态表示是：d[k-1][i][k]<br>
+那么k-&gt;j同理得：d[k-1][k][j]<br>
+所以包含k的点的路径表示为：d[k-1][i][k]+d[k-1][k][j]</p>
+<p>应用：<br>
+1.最短路
+2.传递闭包
+3.找最小环
+4.恰好经过k条边的最短路</p>
+</div></template>
+
+
