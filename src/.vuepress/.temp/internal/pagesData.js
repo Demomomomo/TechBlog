015de87bcc569cc3cda32ae642b8a748b4716f8c
Demomomomo <2889890021@qq.com>
2023-03-27 21:22:17 +0800

    1

--- src/.vuepress/.temp/internal/pagesData.js
+++ src/.vuepress/.temp/internal/pagesData.js
@@ -5,12 +5,16 @@ export const pagesData = {
   "v-70bc2959": () => import(/* webpackChunkName: "v-70bc2959" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/index.html.js").then(({ data }) => data),
   // path: /solution/
   "v-799e9206": () => import(/* webpackChunkName: "v-799e9206" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/index.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/ac.html
-  "v-1dd6d9a3": () => import(/* webpackChunkName: "v-1dd6d9a3" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/ac.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/kmp.html
-  "v-2a37981f": () => import(/* webpackChunkName: "v-2a37981f" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/kmp.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/manachar.html
-  "v-057f5b88": () => import(/* webpackChunkName: "v-057f5b88" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/manachar.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/beibao.html
+  "v-528eea3d": () => import(/* webpackChunkName: "v-528eea3d" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/beibao.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/qu-jian-dp.html
+  "v-17cef7a3": () => import(/* webpackChunkName: "v-17cef7a3" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/qu-jian-dp.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/shu-zi-san-jiao-xing.html
+  "v-c64fcaca": () => import(/* webpackChunkName: "v-c64fcaca" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/shu-zi-san-jiao-xing.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/xian-xing.html
+  "v-1cd7fa5c": () => import(/* webpackChunkName: "v-1cd7fa5c" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/xian-xing.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/zui-chang-shang-sheng.html
+  "v-dc6d973e": () => import(/* webpackChunkName: "v-dc6d973e" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/zui-chang-shang-sheng.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/cha-fen-yue-shu.html
   "v-7aebf5c4": () => import(/* webpackChunkName: "v-7aebf5c4" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/cha-fen-yue-shu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/dan-yuan-zui-duan-lu.html
@@ -21,6 +25,8 @@ export const pagesData = {
   "v-27f3ede8": () => import(/* webpackChunkName: "v-27f3ede8" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/er-fen-tu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/lca.html
   "v-8eb1b37e": () => import(/* webpackChunkName: "v-8eb1b37e" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/lca.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%9B%BE%E8%AE%BA/ou-la-hui-lu.html
+  "v-7678e2dc": () => import(/* webpackChunkName: "v-7678e2dc" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/ou-la-hui-lu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/qiang-lian-tong-fen-liang.html
   "v-b34b0726": () => import(/* webpackChunkName: "v-b34b0726" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/qiang-lian-tong-fen-liang.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/shu-ji-chu.html
@@ -31,14 +37,12 @@ export const pagesData = {
   "v-4497620a": () => import(/* webpackChunkName: "v-4497620a" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/tuo-pu-paixu.html.js").then(({ data }) => data),
   // path: /algorithm/%E5%9B%BE%E8%AE%BA/zui-xiao-sheng-cheng-shu.html
   "v-a8267624": () => import(/* webpackChunkName: "v-a8267624" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/图论/zui-xiao-sheng-cheng-shu.html.js").then(({ data }) => data),
-  // path: /algorithm/%E6%95%B0%E8%AE%BA/ji-chu-zhi-shi.html
-  "v-18012017": () => import(/* webpackChunkName: "v-18012017" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/ji-chu-zhi-shi.html.js").then(({ data }) => data),
-  // path: /algorithm/%E6%95%B0%E8%AE%BA/ksm.html
-  "v-f7e0ba02": () => import(/* webpackChunkName: "v-f7e0ba02" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/ksm.html.js").then(({ data }) => data),
-  // path: /algorithm/%E6%95%B0%E8%AE%BA/zu-he-shu.html
-  "v-9b731bbc": () => import(/* webpackChunkName: "v-9b731bbc" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/zu-he-shu.html.js").then(({ data }) => data),
-  // path: /algorithm/%E6%90%9C%E7%B4%A2/bfs.html
-  "v-0b2145da": () => import(/* webpackChunkName: "v-0b2145da" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/搜索/bfs.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/ac.html
+  "v-1dd6d9a3": () => import(/* webpackChunkName: "v-1dd6d9a3" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/ac.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/kmp.html
+  "v-2a37981f": () => import(/* webpackChunkName: "v-2a37981f" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/kmp.html.js").then(({ data }) => data),
+  // path: /algorithm/%E5%AD%97%E7%AC%A6%E4%B8%B2/manachar.html
+  "v-057f5b88": () => import(/* webpackChunkName: "v-057f5b88" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/字符串/manachar.html.js").then(({ data }) => data),
   // path: /algorithm/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/bing-cha-ji.html
   "v-8e8bac92": () => import(/* webpackChunkName: "v-8e8bac92" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数据结构/bing-cha-ji.html.js").then(({ data }) => data),
   // path: /algorithm/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/fen-kuai.html
@@ -55,52 +59,38 @@ export const pagesData = {
   "v-7a1f0214": () => import(/* webpackChunkName: "v-7a1f0214" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数据结构/xian-duan-shu.html.js").then(({ data }) => data),
   // path: /algorithm/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/zhu-xi-shu.html
   "v-55889154": () => import(/* webpackChunkName: "v-55889154" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数据结构/zhu-xi-shu.html.js").then(({ data }) => data),
+  // path: /algorithm/%E6%90%9C%E7%B4%A2/bfs.html
+  "v-0b2145da": () => import(/* webpackChunkName: "v-0b2145da" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/搜索/bfs.html.js").then(({ data }) => data),
+  // path: /algorithm/%E6%95%B0%E8%AE%BA/ji-chu-zhi-shi.html
+  "v-18012017": () => import(/* webpackChunkName: "v-18012017" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/ji-chu-zhi-shi.html.js").then(({ data }) => data),
+  // path: /algorithm/%E6%95%B0%E8%AE%BA/ksm.html
+  "v-f7e0ba02": () => import(/* webpackChunkName: "v-f7e0ba02" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/ksm.html.js").then(({ data }) => data),
+  // path: /algorithm/%E6%95%B0%E8%AE%BA/zu-he-shu.html
+  "v-9b731bbc": () => import(/* webpackChunkName: "v-9b731bbc" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/数论/zu-he-shu.html.js").then(({ data }) => data),
   // path: /algorithm/%E8%AE%A1%E7%AE%97%E5%87%A0%E4%BD%95/chaji.html
   "v-624abbbe": () => import(/* webpackChunkName: "v-624abbbe" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/计算几何/chaji.html.js").then(({ data }) => data),
   // path: /algorithm/%E8%AE%A1%E7%AE%97%E5%87%A0%E4%BD%95/tu-bao.html
   "v-ccec4096": () => import(/* webpackChunkName: "v-ccec4096" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/计算几何/tu-bao.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/beibao.html
-  "v-528eea3d": () => import(/* webpackChunkName: "v-528eea3d" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/beibao.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/qu-jian-dp.html
-  "v-17cef7a3": () => import(/* webpackChunkName: "v-17cef7a3" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/qu-jian-dp.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/shu-zi-san-jiao-xing.html
-  "v-c64fcaca": () => import(/* webpackChunkName: "v-c64fcaca" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/shu-zi-san-jiao-xing.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/xian-xing.html
-  "v-1cd7fa5c": () => import(/* webpackChunkName: "v-1cd7fa5c" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/xian-xing.html.js").then(({ data }) => data),
-  // path: /algorithm/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/zui-chang-shang-sheng.html
-  "v-dc6d973e": () => import(/* webpackChunkName: "v-dc6d973e" */"E:/TechBlog/src/.vuepress/.temp/pages/algorithm/动态规划/zui-chang-shang-sheng.html.js").then(({ data }) => data),
+  // path: /solution/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/bei-bao0.html
+  "v-7a3c5c44": () => import(/* webpackChunkName: "v-7a3c5c44" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/动态规划/bei-bao0.html.js").then(({ data }) => data),
   // path: /solution/%E5%9B%BE%E8%AE%BA/shu.html
   "v-3318fe32": () => import(/* webpackChunkName: "v-3318fe32" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/图论/shu.html.js").then(({ data }) => data),
   // path: /solution/%E5%9B%BE%E8%AE%BA/zui-duan-lu.html
   "v-f78f0f64": () => import(/* webpackChunkName: "v-f78f0f64" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/图论/zui-duan-lu.html.js").then(({ data }) => data),
-  // path: /solution/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/bei-bao0.html
-  "v-7a3c5c44": () => import(/* webpackChunkName: "v-7a3c5c44" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/动态规划/bei-bao0.html.js").then(({ data }) => data),
-  // path: /solution/%E6%80%9D%E7%BB%B4/D.%20Buying%20gifts.html
-  "v-4a0c9776": () => import(/* webpackChunkName: "v-4a0c9776" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/思维/D. Buying gifts.html.js").then(({ data }) => data),
   // path: /solution/%E5%AD%97%E7%AC%A6%E4%B8%B2/A-ST%20and%20Ts.html
   "v-3290ded0": () => import(/* webpackChunkName: "v-3290ded0" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/字符串/A-ST and Ts.html.js").then(({ data }) => data),
   // path: /solution/%E5%AD%97%E7%AC%A6%E4%B8%B2/kmp.html
   "v-59ad23c3": () => import(/* webpackChunkName: "v-59ad23c3" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/字符串/kmp.html.js").then(({ data }) => data),
   // path: /solution/%E5%AD%97%E7%AC%A6%E4%B8%B2/zi-fu-chuan.html
   "v-07f2a534": () => import(/* webpackChunkName: "v-07f2a534" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/字符串/zi-fu-chuan.html.js").then(({ data }) => data),
+  // path: /solution/%E6%80%9D%E7%BB%B4/D.%20Buying%20gifts.html
+  "v-4a0c9776": () => import(/* webpackChunkName: "v-4a0c9776" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/思维/D. Buying gifts.html.js").then(({ data }) => data),
   // path: /solution/%E6%90%9C%E7%B4%A2/+-.html
   "v-789f38b2": () => import(/* webpackChunkName: "v-789f38b2" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/+-.html.js").then(({ data }) => data),
   // path: /solution/%E6%90%9C%E7%B4%A2/bfs.html
   "v-267368ef": () => import(/* webpackChunkName: "v-267368ef" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/bfs.html.js").then(({ data }) => data),
   // path: /solution/%E6%90%9C%E7%B4%A2/dfs.html
   "v-13947aa6": () => import(/* webpackChunkName: "v-13947aa6" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/搜索/dfs.html.js").then(({ data }) => data),
-  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/ping-heng-shu.html
-  "v-2302f78c": () => import(/* webpackChunkName: "v-2302f78c" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/ping-heng-shu.html.js").then(({ data }) => data),
-  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/shu-zhuang-shu-zu.html
-  "v-218f384a": () => import(/* webpackChunkName: "v-218f384a" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/shu-zhuang-shu-zu.html.js").then(({ data }) => data),
-  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/stl.html
-  "v-49a47336": () => import(/* webpackChunkName: "v-49a47336" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/stl.html.js").then(({ data }) => data),
-  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/xian-duan-shu.html
-  "v-279cebf0": () => import(/* webpackChunkName: "v-279cebf0" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/xian-duan-shu.html.js").then(({ data }) => data),
-  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/zhu-xi-shu.html
-  "v-394b5610": () => import(/* webpackChunkName: "v-394b5610" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/zhu-xi-shu.html.js").then(({ data }) => data),
-  // path: /solution/%E6%9D%82%E8%AE%B0/bi-ji.html
-  "v-1d691529": () => import(/* webpackChunkName: "v-1d691529" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/杂记/bi-ji.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E5%AD%A6/ai-aj-ak.html
   "v-61d13f86": () => import(/* webpackChunkName: "v-61d13f86" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数学/ai-aj-ak.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E5%AD%A6/C.%20Serval%20and%20Toxel's%20Arrays.html
@@ -115,6 +105,18 @@ export const pagesData = {
   "v-03e26e17": () => import(/* webpackChunkName: "v-03e26e17" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数学/tong-yu.html.js").then(({ data }) => data),
   // path: /solution/%E6%95%B0%E5%AD%A6/triangle.html
   "v-79322004": () => import(/* webpackChunkName: "v-79322004" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数学/triangle.html.js").then(({ data }) => data),
+  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/ping-heng-shu.html
+  "v-2302f78c": () => import(/* webpackChunkName: "v-2302f78c" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/ping-heng-shu.html.js").then(({ data }) => data),
+  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/shu-zhuang-shu-zu.html
+  "v-218f384a": () => import(/* webpackChunkName: "v-218f384a" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/shu-zhuang-shu-zu.html.js").then(({ data }) => data),
+  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/stl.html
+  "v-49a47336": () => import(/* webpackChunkName: "v-49a47336" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/stl.html.js").then(({ data }) => data),
+  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/xian-duan-shu.html
+  "v-279cebf0": () => import(/* webpackChunkName: "v-279cebf0" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/xian-duan-shu.html.js").then(({ data }) => data),
+  // path: /solution/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84/zhu-xi-shu.html
+  "v-394b5610": () => import(/* webpackChunkName: "v-394b5610" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/数据结构/zhu-xi-shu.html.js").then(({ data }) => data),
+  // path: /solution/%E6%9D%82%E8%AE%B0/bi-ji.html
+  "v-1d691529": () => import(/* webpackChunkName: "v-1d691529" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/杂记/bi-ji.html.js").then(({ data }) => data),
   // path: /solution/%E6%9E%84%E9%80%A0/a-b-pi.html
   "v-b60648be": () => import(/* webpackChunkName: "v-b60648be" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/构造/a-b-pi.html.js").then(({ data }) => data),
   // path: /solution/%E6%9E%84%E9%80%A0/beishu.html
@@ -141,14 +143,14 @@ export const pagesData = {
   "v-5f8a0336": () => import(/* webpackChunkName: "v-5f8a0336" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/算法技巧/pai-xu.html.js").then(({ data }) => data),
   // path: /solution/%E7%AE%97%E6%B3%95%E6%8A%80%E5%B7%A7/si-wei.html
   "v-06000b64": () => import(/* webpackChunkName: "v-06000b64" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/算法技巧/si-wei.html.js").then(({ data }) => data),
+  // path: /solution/%E8%AE%A1%E7%AE%97%E5%87%A0%E4%BD%95/ji-suan-ji-he.html
+  "v-0826d81a": () => import(/* webpackChunkName: "v-0826d81a" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/计算几何/ji-suan-ji-he.html.js").then(({ data }) => data),
   // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/jie-gou-ti-pai-xu.html
   "v-7c8a5a6c": () => import(/* webpackChunkName: "v-7c8a5a6c" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/jie-gou-ti-pai-xu.html.js").then(({ data }) => data),
   // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/map.html
   "v-187be613": () => import(/* webpackChunkName: "v-187be613" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/map.html.js").then(({ data }) => data),
   // path: /solution/%E8%AF%AD%E6%B3%95%E6%8A%80%E5%B7%A7/stl.html
   "v-6e05f6a4": () => import(/* webpackChunkName: "v-6e05f6a4" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/语法技巧/stl.html.js").then(({ data }) => data),
-  // path: /solution/%E8%AE%A1%E7%AE%97%E5%87%A0%E4%BD%95/ji-suan-ji-he.html
-  "v-0826d81a": () => import(/* webpackChunkName: "v-0826d81a" */"E:/TechBlog/src/.vuepress/.temp/pages/solution/计算几何/ji-suan-ji-he.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"E:/TechBlog/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
   // path: /category/
@@ -171,10 +173,10 @@ export const pagesData = {
   "v-93cb9d6e": () => import(/* webpackChunkName: "v-93cb9d6e" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/字符串/index.html.js").then(({ data }) => data),
   // path: /tag/%E6%90%9C%E7%B4%A2/
   "v-3b0e2518": () => import(/* webpackChunkName: "v-3b0e2518" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/搜索/index.html.js").then(({ data }) => data),
-  // path: /tag/note/
-  "v-28e08cef": () => import(/* webpackChunkName: "v-28e08cef" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/note/index.html.js").then(({ data }) => data),
   // path: /tag/%E6%95%B0%E5%AD%A6/
   "v-9904a07c": () => import(/* webpackChunkName: "v-9904a07c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/数学/index.html.js").then(({ data }) => data),
+  // path: /tag/note/
+  "v-28e08cef": () => import(/* webpackChunkName: "v-28e08cef" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/note/index.html.js").then(({ data }) => data),
   // path: /tag/acm/
   "v-b316775c": () => import(/* webpackChunkName: "v-b316775c" */"E:/TechBlog/src/.vuepress/.temp/pages/tag/acm/index.html.js").then(({ data }) => data),
   // path: /tag/%E6%8A%80%E5%B7%A7/
---
+++ src/algorithm/图论/ou-la-hui-lu.md
@@ -0,0 +1,286 @@
+---
+title: 欧拉路径和欧拉回路
+---
+
+## 概念
+
+欧拉路径：是否存在一条路径使得每条边只走一次  
+欧拉回路：是否存在一条路径使得每条边只走一次，且起点和终点相同  
+
+观察每个点的度数和（出度+入度），对于一个起点来说，一条边出，以后再经过这个点的时候必定一进一出。那么对于起点的度实际上是奇数。同理对于终点，一条边入，之前经过这个点都是一进一出，度数也是奇数。  
+
+那么对于中间点来说，进去一次就需要出来一次，度数应该是偶数。  
+
+
+## 对无向图
+前提条件：所有边都是联通的  
+存在欧拉路径的充分必要条件：度数为奇数的点有0个或者两个。  
+存在欧拉回路的充分必要条件：所有点的度数全为偶数  
+
+## 对有向图
+前提条件：所有边都是联通的（点可以不联通但是边必须要联通）  
+存在欧拉路径的充分必要条件：要么所有点的出度均等于入度，要么有一个点的入度比出度多一，有一个点的入度比出度多一，其他的点入度等于出度。  
+存在欧拉回路的充分必要条件：所有点的出度均等于入度  
+
+## 思路
+路径可以看做是由两条连向起点和终点的路径加上一堆环组成。因为满足每个点的度数是偶数的话，在出去这个点之后肯定需要再回来。那么就构成了一个环。  
+
+如何将点和路融合？  
+
+那么在求欧拉路径的时候，用dfs遍历每条路。在遍历到u结点的时候，遍历完他的每条出边之后再将他加入队列，那么和u相关的所有边都被遍历了一遍，相当于走了以u为顶点的环。那么这样得到的队列其实是终点到起点的顺序，那么将他倒序就可以了。  
+
+
+## 例题
+
+### 欧拉回路
+原题链接：  
+https://www.acwing.com/problem/content/1186/  
+
+题意：  
+给你一个有向图或者无向图，找出他的一条欧拉回路。  
+
+思路：  
+先判断度数是否符合  
+对于无向图来说，所有出度入度的和必须全是偶数  
+对于有向图来说，出度必须等于入度。  
+
+判断完之后再从一个有边的点开始遍历，用cnt记录走了几条边，如果cnt少于总边数，那么说明边不联通，没有欧拉回路。  
+
+之后再倒序输出走过的点就可以了。  
+
+走到当前u的时候，我们需要遍历与他相连的每条边。  
+
+边的编号：对于有向图来说下标就是他的编号-1。对于无向图来说，因为又加了一个反向边，所以应该/2+1.  
+
+对于一条边，如果走过的话我们将他删掉。如果没有走过，那么我们将这个边标记并删掉，如果这个图是无向图，那么我们还需要删除他的反向边。然后遍历这个点，当遍历完的时候将这个边加入答案数组中。  
+
+```cpp
+#include<bits/stdc++.h>
+using namespace std;
+const int N=4e5+10;
+int n,m,t;
+int e[N],ne[N],idx,h[N];
+bool used[N];
+vector<int> ans;
+int chu[N],ru[N];
+void add(int a,int b){
+	e[idx]=b;
+	ne[idx]=h[a];
+	h[a]=idx++;
+}
+void dfs(int u){
+	for(int &i=h[u];~i;){
+		if(used[i]){
+			i=ne[i];
+			continue;
+		}
+		used[i]=true;
+		if(t==1) used[i^1]=true;
+		int id;
+		if(t==1){
+			id=i/2+1;
+			if(i&1) id=-id;
+		}else{
+			id=i+1;
+		}
+		int j=e[i];
+		i=ne[i];
+		dfs(j);
+		ans.push_back(id); 
+	}
+}
+int main(){
+	cin>>t>>n>>m;
+	memset(h,-1,sizeof h);
+	for(int i=1;i<=m;i++){
+		int a,b;
+		cin>>a>>b;
+		add(a,b);
+		ru[b]++;
+		chu[a]++;
+		if(t==1) add(b,a);
+	}
+	if(t==1){
+		for(int i=1;i<=n;i++){
+			if((ru[i]+chu[i])%2){
+				cout<<"NO";
+				return 0;
+			}
+		}
+	}else{
+		for(int i=1;i<=n;i++){
+			if(ru[i]!=chu[i]){
+				cout<<"NO";
+				return 0;
+			}
+		}
+	}
+	
+	for(int i=1;i<=n;i++){
+		if(~h[i]){
+			dfs(i);
+			break;
+		}
+	}
+	
+	if(ans.size() <m){
+		cout<<"NO";
+		return 0;
+	}
+	
+	cout<<"YES"<<endl;
+	for(int i=ans.size() -1;i>=0;i--){
+		cout<<ans[i]<<" ";
+	}
+	return 0;
+}
+
+```
+
+
+
+### 骑马修栅栏
+原题链接：  
+https://www.acwing.com/problem/content/1126/  
+题意：  
+有不超过500个农场，有m个栅栏，需要将所有栅栏维修，且每个栅栏只经过一次（保证一定有解），求经过的点字典序最小的路径。  
+思路：  
+对于一个u点来说，我们如果将与他相连的点从小到大遍历边，并且在遍历完之后最后将u加进去，那么大的点肯定会比小的点先键入队中。而且我们求的是路径的逆序，那么我们按照这个方案逆序输出的点实际上就是点按字典序最小的路径。  
+那么因为可以在任何点出发任何点结束，那么我们先判断有没有度数是奇数的点，如果有的话从最先出现的度数为奇数的点开始遍历，如果没有的话就从最先出现的度数不为0的点开始遍历。  
+对于每个点u，从小到大遍历完他的所有边再将他加进去。  
+
+```cpp
+#include<bits/stdc++.h>
+using namespace std;
+int m;
+int g[600][600];
+vector<int> ans;
+
+void dfs(int u){
+	for(int i=1;i<=500;i++){
+		while(g[u][i]){
+			g[u][i]--;
+			g[i][u]--;
+			dfs(i);
+		}
+	}
+	ans.push_back(u); 
+}
+int d[600];
+int main(){
+	cin>>m;
+	while(m--){
+		int a,b;
+		cin>>a>>b;
+		g[a][b]++;
+		g[b][a]++;
+		d[a]++;
+		d[b]++;
+	}
+	int s=1;
+	while(!d[s]) s++;
+	for(int i=1;i<=500;i++) {
+		if(d[i]&1){
+			s=i;
+			break;
+		}
+	}
+	dfs(s);
+	for(int i=ans.size() -1;i>=0;i--) cout<<ans[i]<<endl;
+}
+
+```
+
+### 单词游戏
+原题链接：  
+https://www.acwing.com/problem/content/1187/  
+
+
+题意：  
+有n个-盘子，每个盘子里装一个由小写字母组成的盘子，需要给这些盘子找到一个合适的顺序，使得相邻的两个盘子中，前一个盘子上的单词末尾的字母等于后一个单词的首字母。  
+
+思路：  
+把每个单词看成是一个由首字母连向末字母的一条边，那么要求的是这些边是否存在欧拉路径。  
+有欧拉路径的条件：  
+1.所有边联通  
+2.除了起点和终点的点其他的点的出度等于入度，起点的出度比入度多1，终点的入度比出度多1.  
+第一个条件可以用并查集来维护，找到所有出现的字母，看看他们的集合数是不是只有一个。  
+```cpp
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int ru[30],chu[30];
+int p[30];
+int cnt;
+bool st[30];
+int find(int x){
+	if(p[x]!=x) p[x]=find(p[x]);
+	return p[x];
+}
+void sove(){
+	cin>>n;
+	for(int i=0;i<26;i++)p[i]=i;
+	memset(ru,0,sizeof ru);
+	memset(chu,0,sizeof chu);
+	memset(st,false,sizeof st);
+	for(int i=1;i<=n;i++){
+		string s;
+		cin>>s;
+		int op1=s[0]-'a';
+		int len=s.size();
+		int op2=s[len-1]-'a';
+		st[op1]=st[op2]=true;
+		if(find(op1)!=find(op2)){
+			p[find(op1)]=find(op2);
+		}
+		chu[op1]++;
+		ru[op2]++;
+	}
+	int con1=0,con2=0,con3=0;
+	bool f=true;
+	for(int i=0;i<26;i++){
+		if(chu[i]==ru[i]){
+			con3++;
+		}else
+		if(chu[i]==ru[i]+1){
+			con1++;
+		}else
+		if(ru[i]==chu[i]+1){
+			con2++;
+		}else{
+			f=false;
+		}
+	}
+	if(!f) {
+		cout<<"The door cannot be opened."<<endl;
+		return ;
+	}
+	int con=0;
+	for(int i=0;i<26;i++){
+		if(p[i]==i&&st[i]) con++;
+	}
+	if(con!=1){
+		cout<<"The door cannot be opened."<<endl;
+		return ;
+	}
+	if((con1==1&&con2==1)||(con1==0&&con2==0)){
+		cout<<"Ordering is possible."<<endl;
+	}else cout<<"The door cannot be opened."<<endl;
+}
+
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		sove();
+	}
+	
+	return 0;
+}
+```
+
+
+
+
+
